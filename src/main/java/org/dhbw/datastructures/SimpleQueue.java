194469204b2bea96299431b7e419647b704b7c7d
Flash3r01 <44031456+Flash3r01@users.noreply.github.com>
2023-05-04 01:07:06 +0200

    Add comments to existing classes. - Remove unneeded classes

--- src/main/java/org/dhbw/datastructures/SimpleQueue.java
+++ src/main/java/org/dhbw/datastructures/SimpleQueue.java
@@ -3,14 +3,27 @@ package org.dhbw.datastructures;
 import org.dhbw.NotYetImplementedException;
 
 public class SimpleQueue {
+    /**
+     * Adds an element to the back of the queue.
+     *
+     * @param element The element to add
+     */
     void add(String element) {
         throw new NotYetImplementedException();
     }
 
+    /**
+     * Removes the next element from the queue and returns it.
+     *
+     * @return The element at the front of the queue.
+     */
     String poll() {
         throw new NotYetImplementedException();
     }
 
+    /**
+     * @return The number of elements in the queue.
+     */
     int size() {
         throw new NotYetImplementedException();
     }
--- src/main/java/org/dhbw/datastructures/SimpleStack.java
+++ src/main/java/org/dhbw/datastructures/SimpleStack.java
@@ -3,14 +3,27 @@ package org.dhbw.datastructures;
 import org.dhbw.NotYetImplementedException;
 
 public class SimpleStack {
+    /**
+     * Adds a new element to the top of the stack.
+     *
+     * @param element The element to add to the stack.
+     */
     void push(String element) {
         throw new NotYetImplementedException();
     }
 
+    /**
+     * Removes the next element from the top of the stack and returns it.
+     *
+     * @return The element at the top of the stack.
+     */
     String pop() {
         throw new NotYetImplementedException();
     }
 
+    /**
+     * @return The number of elements on the stack.
+     */
     int size() {
         throw new NotYetImplementedException();
     }
